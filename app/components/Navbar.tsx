e86ef729fa74c142a92820fda7b14b40106909d2
Sameea Saqee <sameea.saqee@gmail.com>
2025-07-04 12:45:36 +0500

    Added sidebar and navbar with styling

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
-import { BsFillMicFill } from "react-icons/bs";
 import { FiSearch } from "react-icons/fi";
 import { FiMenu } from "react-icons/fi";
 import { CiBellOn } from "react-icons/ci";
-import { HiOutlinePlus } from "react-icons/hi";
+import { BsPlusLg } from "react-icons/bs";
+import { VscMicFilled } from "react-icons/vsc";
 
 export default function Navbar() {
     return (
@@ -12,18 +12,26 @@ export default function Navbar() {
                 <FiMenu />
                 <Image src="/youtube-logo.png" alt="youtube logo" width={100} height={100} />
             </div>
-            <div className="flex items-center gap-5 w-full max-w-2xl mx-auto">
+            <div className="flex items-center gap-5 mx-auto">
                 <button className="flex items-center border border-gray-300 rounded-full overflow-hidden">
-                    <input type="text" placeholder="Search" className="font-semibold px-4 py-2 focus:outline-none h-10 w-200 border border-gray-300" />
-                    <FiSearch className="text-3xl cursor-pointer" />
+                    <input
+                        type="text"
+                        placeholder="Search"
+                        className="pl-4 py-2 h-10 w-140 focus:outline-none"
+                    />
+                    <div className="bg-gray-200 h-10 flex items-center px-4">
+                        <FiSearch className="text-xl text-gray-700 cursor-pointer" />
+                    </div>
                 </button>
-                <button className="flex items-center justify-center rounded-full bg-gray-100 hover:bg-gray-200 w-15 h-10 cursor-pointer">
-                    <BsFillMicFill className="text-xl" />
+
+                <button className="rounded-full bg-gray-100 hover:bg-gray-200 w-10 h-10 flex items-center justify-center">
+                    <VscMicFilled  className="text-xl" />
                 </button>
             </div>
+
             <div className="flex items-center gap-6">
-                <button className="flex items-center border border-gray-200 bg-gray-200 font-semibold text-sm rounded-full hover:bg-gray-200 h-8 w-20 px-6 cursor-pointer">
-                    <HiOutlinePlus />
+                <button className="flex items-center border border-gray-200 bg-gray-200 font-semibold text-sm rounded-full hover:bg-gray-200 h-8 w-auto px-4 gap-2 cursor-pointer">
+                    <BsPlusLg className="" />
                     Create
                 </button>
                 <CiBellOn className="cursor-pointer rounded-full hover:bg-gray-200 h-6 w-6" />
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,52 @@
 import Image from "next/image";
+import Link from "next/link";
+import { IoMdHome } from "react-icons/io";
+import { SiYoutubeshorts } from "react-icons/si";
+import { MdOutlineSubscriptions } from "react-icons/md";
+import { GoHistory } from "react-icons/go";
+import { CgPlayList } from "react-icons/cg";
+import { MdOutlineWatchLater } from "react-icons/md";
+import { BiLike } from "react-icons/bi";
+import { TbMathGreater } from "react-icons/tb";
+
 
 export default function Home() {
   return (
-    <h1></h1>
+    <div className="flex flex-row">
+      <div className="flex flex-col w-1/6 h-auto">
+        <Link href="/" className="sidebar"><IoMdHome className="size-5" />Home</Link>
+        <Link href="/" className="sidebar"><SiYoutubeshorts className="size-5" />
+          Shorts</Link>
+        <Link href="/" className="sidebar"><MdOutlineSubscriptions className="size-5" />
+          Subscription</Link><hr className=" border-gray-300 mt-3" />
+        <Link href="/" className="sidebar">You<TbMathGreater
+          className="size-3 text-black-900" />
+        </Link>
+        <Link href="/" className="sidebar"><GoHistory className="size-5" />
+          History</Link>
+        <Link href="/" className="sidebar"><CgPlayList className="size-5" />
+          Playlists</Link>
+        <Link href="/" className="sidebar"><MdOutlineWatchLater className="size-5" />
+          Watch later</Link>
+        <Link href="/" className="sidebar"><BiLike className="size-5" />
+          Liked videos</Link><hr className=" border-gray-300 mt-3" />
+        <Link href="/" className="sidebar">Subscriptions</Link>
+      </div>
+      <div className="w-5/6 h-auto pt-3">
+        <div className="flex justify-evenly">
+          <Link href="/" className="navbar">All</Link>
+          <Link href="/" className="navbar">News</Link>
+          <Link href="/" className="navbar">JavaScript</Link>
+          <Link href="/" className="navbar">Music</Link>
+          <Link href="/" className="navbar">Television series</Link>
+          <Link href="/" className="navbar">Live</Link>
+          <Link href="/" className="navbar">Visual arts</Link>
+          <Link href="/" className="navbar">Cartoons</Link>
+          <Link href="/" className="navbar">Cars</Link>
+          <Link href="/" className="navbar">Watched</Link>
+        </div>
+        <div></div>
+      </div>
+    </div>
   );
 }
